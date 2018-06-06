572d9db19f06ce1bbe209cbdf9cd9130be1d887d
kotsis <kotsis@users.noreply.github.com>
2018-06-06 22:54:36 +0300

    Code cleanup

--- phpword2text.php
+++ phpword2text.php
@@ -7,24 +7,17 @@ class PHPWord2Text {
 	protected function getElementsText($elements, $finish_char=' '){
 		$retText = '';
 		foreach($elements as $el){
-			//echo "        Element: $j\n";
-			
 			if(method_exists($el,'getText')){
 				$retText .= $el->getText().$finish_char;
 			}
 			else{
-				//if($el instanceof \PhpOffice\PhpWord\Element\AbstractContainer){
 				if(method_exists($el,'getElements')){
-					//echo "        text: getting elements for class:".get_class($el)."\n";
 					if(get_class($el) == 'PhpOffice\PhpWord\Element\TextRun'){
 						$retText .= getElementsText($el->getElements(),'')." ";
 					}
 					else{
 						$retText .= getElementsText($el->getElements())." ";
 					}
-				}
-				else{
-					//echo "        text: not available for class:".get_class($el)."\n";
 				}
 			}
 		}
@@ -34,16 +27,16 @@ class PHPWord2Text {
 	public function extractText($filename){
 		$ext = pathinfo($filename, PATHINFO_EXTENSION);
 		if($ext == 'docx'){
-			$phpWord = \PhpOffice\PhpWord\IOFactory::load($filename, 'Word2007'); //gia .docx arxeia
+			$phpWord = \PhpOffice\PhpWord\IOFactory::load($filename, 'Word2007'); //for .docx files
 		}
 		elseif($ext == 'doc'){
-			$phpWord = \PhpOffice\PhpWord\IOFactory::load($filename, 'MsDoc'); //gia .doc arxeia
+			$phpWord = \PhpOffice\PhpWord\IOFactory::load($filename, 'MsDoc'); //for .doc files
 		}
 		elseif($ext == 'rtf'){
-			$phpWord = \PhpOffice\PhpWord\IOFactory::load($source,'RTF'); //gia RTF arxeia
+			$phpWord = \PhpOffice\PhpWord\IOFactory::load($source,'RTF'); //for RTF files
 		}
 		elseif($ext == 'odt'){
-			$phpWord = \PhpOffice\PhpWord\IOFactory::load($source, 'ODText'); //gia .odt arxeia
+			$phpWord = \PhpOffice\PhpWord\IOFactory::load($source, 'ODText'); //for .odt files
 		}
 		else{
 			return ''; //nothing to do here
@@ -55,9 +48,7 @@ class PHPWord2Text {
 		foreach($allSections as $s){
 
 			$allElements = $s->getElements();
-			
-			//printElementsText($allElements, $j);
-			
+
 			$ret .= $this->getElementsText($allElements);
 
 		}
